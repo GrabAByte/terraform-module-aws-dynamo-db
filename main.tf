e89cefd5fa2bfd5d2e3c42c9b681841c71aaa5e3
Tom Richards <twr.devops@gmail.com>
2025-08-08 16:31:54 +0100

    first commit

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
 resource "aws_dynamodb_table" "dynamodb_table" {
   name         = var.name
   billing_mode = var.billing_mode
-  # read_capacity = var.read_capacity
-  # write_capacity = var.write_capacity
-  hash_key  = var.hash_key
-  range_key = var.range_key
+  hash_key     = var.hash_key
+  range_key    = var.range_key
 
   dynamic "attribute" {
     for_each = var.attributes
@@ -13,27 +11,6 @@ resource "aws_dynamodb_table" "dynamodb_table" {
       type = attribute.value.type
     }
   }
-
-  #dynamic "ttl" {
-  #  for_each = var.ttl
-  #  content {
-  #    attribute_name = ttl.value.attribute_name
-  #    enabled        = ttl.value.enabled
-  #  }
-  #}
-
-  #dynamic "global_secondary_index" {
-  #  for_each = var.gsi
-  #  content {
-  #    name               = global_secondary_index.value.name
-  #    hash_key           = global_secondary_index.value.hash_key
-  #    range_key          = lookup(global_secondary_index.value, "range_key", null)
-  #    projection_type    = global_secondary_index.value.projection_type
-  #    non_key_attributes = lookup(global_secondary_index.value, "non_key_attributes", null)
-  #    read_capacity      = lookup(global_secondary_index.value, "read_capacity", null)
-  #    write_capacity     = lookup(global_secondary_index.value, "write_capacity", null)
-  #  }
-  #}
 
   tags = var.tags
 }
--- variables.tf
+++ variables.tf
@@ -6,14 +6,6 @@ variable "billing_mode" {
   type = string
 }
 
-# variable "read_capacity" {
-#   type = string
-# }
-
-# variable "write_capacity" {
-#   type = string
-# }
-
 variable "hash_key" {
   type = string
 }
@@ -29,27 +21,6 @@ variable "attributes" {
   }))
   description = "List of DynamoDB table attributes"
 }
-
-#variable "ttl" {
-#  type = list(object({
-#    attribute_name = string
-#    enabled        = bool
-#  }))
-#  description = "TTL attributes"
-#}
-
-#variable "gsi" {
-#  type = list(object({
-#    name               = string
-#    hash_key           = string
-#    range_key          = optional(string)
-#    projection_type    = string
-#    non_key_attributes = optional(list(string))
-#    #read_capacity      = optional(number)
-#    write_capacity = optional(number)
-#  }))
-#  description = "List of global secondary indexes for the DynamoDB table"
-#}
 
 variable "tags" {
   type = map(any)
